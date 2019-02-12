46510a317ac67e7af2053171e5542a6896632f27
rubinus.chu <rubinus.chu@gmail.com>
2019-02-12 20:22:11 +0800

    add lottery

---
+++ lottery/web/controllers/index_lucky_5check_blackuser.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"time"
+
+	"mygo/lottery/models"
+	"mygo/lottery/services"
+)
+
+func (api *LuckyApi) checkBlackUser(uid int) (bool, *models.LtUser) {
+	info := services.NewUserService().Get(uid)
+	if info != nil && info.Blacktime > int(time.Now().Unix()) {
+		// 黑名单存在并且有效
+		return false, info
+	}
+	return true, info
+}
